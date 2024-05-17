35a56c9425c24ca46f740926c524aa0d11522fd7
Aiman Fadillah <mlakece914@gmail.com>
2024-05-17 22:04:37 +0700

    speed insight

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
   import {RouterView } from 'vue-router'
+  import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <template>
+  <SpeedInsights />
   <RouterView />
 </template>
